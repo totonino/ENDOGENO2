043976906a373d7d0f23df10f6417f1fb2c4808a
totonino <juantorresvillarroel@yahoo.es>
2015-01-28 23:20:54 -0300

    ENDOGENO
    WEB DISEÑO

---
+++ php/verificacion.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web PHP</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="/WEB ENDOGENO/css/estilos.css"/>
+
+	</head>
+	<body>	
+		
+		<?php  
+			echo "<p><strong>Este texto está escrito con PHP</strong></p>";
+		?>
+		
+<br><a href="/WEB ENDOGENO/index.html"><input type="submit" value="Iniciar Sesión" /></a>
+</body>
+</html>
---
+++ sql/basedatos_1.sql
@@ -0,0 +1,10 @@
+-- Tabla para los Clientes --
+
+CREATE TABLE 'clientes' (
+'id_cliente' INT(5) NOT NULL AUTO_INCREMENT PRYMARY KEY,
+'nombre' VARCHAR(75) NOT NULL, 
+
+
+
+)
+
